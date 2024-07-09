38856d6d7238466c57b014913a99de5a60ce03e6
Adriaen Limborgh <adriaen95@hotmail.com>
2024-07-09 13:35:35 +0200

    chore: Update footer styles and links
    The code changes in this commit update the styles of the footer component to improve its appearance. Additionally, the links in the footer have been updated to use the `Link` component from `react-router-dom` for better user experience and client-side routing.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,40 @@
-// src/components/Footer.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/css/Footer.css';
 
 const Footer = () => {
+  // Function to scroll to the top of the page
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth', // For smooth scrolling
+    });
+  };
+
   return (
     <footer>
       <div className="footer-content">
         <div className="footer-about">
           <h3>Over Mij</h3>
           <p>Welkom op mijn website! Ik ben een Fullstack Developer met een passie voor het oplossen van complexe problemen.</p>
-          <Link to="/about" className="footer-link">Lees meer</Link>
+          <Link 
+            to="about"
+            className="footer-link"
+            smooth={true}
+            duration={500}
+            onClick={scrollToTop} // Add onClick event handler
+          >
+            Lees meer
+          </Link>
         </div>
         <div className="footer-links">
           <h3>Snelkoppelingen</h3>
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">Over mij</Link></li>
-            <li><Link to="/projects">Projecten</Link></li>
-            <li><Link to="/resume">CV</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/" smooth={true} duration={500} onClick={scrollToTop}>Home</Link></li>
+            <li><Link to="about" smooth={true} duration={500} onClick={scrollToTop}>Over mij</Link></li>
+            <li><Link to="projects" smooth={true} duration={500} onClick={scrollToTop}>Projecten</Link></li>
+            <li><Link to="resume" smooth={true} duration={500} onClick={scrollToTop}>CV</Link></li>
+            <li><Link to="contact" smooth={true} duration={500} onClick={scrollToTop}>Contact</Link></li>
           </ul>
         </div>
         <div className="footer-social">
@@ -37,7 +52,6 @@ const Footer = () => {
       <div className="footer-bottom">
         <p><span id="year"></span> Alle rechten voorbehouden.</p>
       </div>
-      <script> js\script.js </script>
     </footer>
   );
 }
