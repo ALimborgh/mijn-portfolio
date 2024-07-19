543c0f9b69f9a97a5a85de902835a50a0ec1b530
Adriaen Limborgh <adriaen95@hotmail.com>
2024-07-19 15:15:55 +0200

    refactor: Add advanced React project images and update project description
    The code changes in this commit add the images for the advanced React project and update the project description in the ProjectsData.js file. The images are imported and added to the imagePaths array, allowing them to be displayed in the project cards. Additionally, the project description is enhanced to provide more detailed information about the purpose and features of the advanced React project. This update improves the visual representation of the project and provides clearer information for users.

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -1,11 +1,16 @@
 // src/data/projectsData.js
 import basicReactProject from '../assets/images/Basic React Project.png';
 import basicReactProject2 from '../assets/images/Basic React Project 2.png';
+import advancedReactProject from '../assets/images/Advanced React Project.png';
+import advancedReactProject2 from '../assets/images/Advanced React Project 2.png';
+import advancedReactProject3 from '../assets/images/Advanced React Project 3.png';
+import advancedReactProject4 from '../assets/images/Advanced React Project 4.png';
+import advancedReactProject5 from '../assets/images/Advanced React Project 5.png';
 
 const projectsData = [
     {
       id: 1,
-      title: 'Basic React Project',
+      title: 'Basic React Project (Schoolopdracht)',
       description: 'Dit project is een essentiële React-component binnen een kookwebsite of -applicatie die is ontworpen om gedetailleerde informatie over een specifiek recept te tonen. Deze component fungeert als de centrale weergavepagina voor gebruikers die meer willen weten over een recept, inclusief ingrediënten, bereidingsinstructies, voedingswaarde en andere relevante gegevens. Ook is het mogelijk om op een van de recepten te klikken om de details van dat recept te bekijken. Verder is er een zoekfunctie toegevoegd om gemakkelijk tussen de verschillende recepten te kunnen zoeken.',
       Toepassingen:['Kookwebsites', 'Dieet- en gezondheidsapps', 'E-learning platforms', 'Persoonlijke receptenbeheer', 'Voedselblogs'],
       technologies: ['HTML', 'CSS', 'JavaScript', 'React', 'React-router', 'Chakra UI'],
@@ -17,9 +22,18 @@ const projectsData = [
     },
     {
       id: 2,
-      title: 'Project 2',
-      description: 'Description of Project 2. This project showcases backend development with Python and Django.',
-      technologies: ['Python', 'Django'],
+      title: 'Advanced React Project (Schoolopdracht)',
+      description: 'Deze React-applicatie is een uitgebreid evenementenbeheersysteem waarmee gebruikers evenementen kunnen bekijken, zoeken, filteren en toevoegen. Gebruikers kunnen een lijst met evenementen zien, inclusief details zoals titel, afbeelding, beschrijving, locatie, begintijd en eindtijd. Via een zoekfunctie kunnen ze evenementen vinden op basis van trefwoorden en filteren op categorieën zoals locatie of tijd. Nieuwe evenementen kunnen eenvoudig worden toegevoegd via een modaal formulier, waarin alle relevante informatie en categorieën worden ingevoerd. De applicatie maakt gebruik van Chakra UI voor een moderne gebruikersinterface en react-router-dom voor naadloze navigatie, met dynamische gegevensopslag en -ophaling van een backend-server voor een interactieve ervaring.',
+      Toepassingen: ['Bedrijfsgebruik', 'Onderwijsinstellingen', 'Gemeenschapsorganisaties', 'Culturele en Recreatieve Gebeurtenissen', 'Persoonlijk Gebruik', 'Commerciële Evenementen'],
+      technologies: ['React', 'Chakra UI', 'react-router-dom', 'Fetch API', 'JSON', 'JavaScript ES6', 'async/await', 'useState', 'useEffect', 'useDisclosure', 'useToast'],
+      imagePaths: [
+        advancedReactProject, 
+        advancedReactProject2,
+        advancedReactProject3,
+        advancedReactProject4,
+        advancedReactProject5
+    ],
+      link: 'https://github.com/ALimborgh/React-Advanced-Project',
     },
     // Add more projects as needed
   ];
