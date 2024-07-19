1332962aad6ae65708f1977364656c564a1a71c8
Adriaen Limborgh <adriaen95@hotmail.com>
2024-07-19 16:30:23 +0200

    refactor: Add emailjs-com npm dependency and implement contact form submission
    The code changes in this commit add the emailjs-com npm dependency and implement the functionality to send a contact form submission. The sendEmail function is added to handle the form submission, preventing the default behavior and using emailjs to send the form data. If the message is sent successfully, an alert is displayed to notify the user. If there is an error, another alert is displayed. This update enhances the Contact component by enabling users to send messages directly from the website.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,27 @@
 // src/components/Contact.js
 import React from 'react';
+import emailjs from 'emailjs-com';
 import '../assets/css/Contact.css';
 
 const Contact = () => {
+  const sendEmail = (e) => {
+    e.preventDefault(); // Prevents default form submission behavior
+
+    emailjs.sendForm('service_bbrbc2m', 'template_13rrfgc', e.target, 'nYSePl5tMfJR0Wwam')
+      .then((result) => {
+          console.log(result.text);
+          alert('Message sent successfully!');
+      }, (error) => {
+          console.log(error.text);
+          alert('Failed to send the message, please try again.');
+      });
+  };
+
   return (
     <main>
       <section>
         <h1>Contact mij</h1>
-        <form>
+        <form onSubmit={sendEmail}>
           <label htmlFor="name">Name:</label>
           <input type="text" id="name" name="name" required /><br /><br />
           
