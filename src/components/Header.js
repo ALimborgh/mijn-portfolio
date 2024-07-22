983b880f971335a48549bc61f1161c1fa559bf9f
Adriaen Limborgh <adriaen95@hotmail.com>
2024-07-22 17:27:59 +0200

    Added easteregg of a triforce

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,30 @@
 // src/components/Header.js
-import React from 'react';
+import React, { useState } from 'react';
 import '../assets/css/Header.css';
+import TriforceSVG from './TriforceSVG';
 import HamburgerMenu from './HamburgerMenu';
 
 const Header = () => {
+  const [name, setName] = useState('Adriaen Limborgh');
+  const [showTriforce, setShowTriforce] = useState(false);
+
+  const handleClick = () => {
+    setShowTriforce(!showTriforce);
+  
+    if (!showTriforce) {
+      // If showTriforce is currently false, change it to true and set the name to TriforceSVG
+      setName(<TriforceSVG />);
+      // Set a timeout to revert the name back to 'Adriaen Limborgh' after 3 seconds (3000 milliseconds)
+      setTimeout(() => {
+        setName('Adriaen Limborgh');
+        setShowTriforce(false); // Ensure showTriforce is set back to false
+      }, 3000);
+    }
+  };
+
   return (
     <header>
-      <h1>Adriaen Limborgh</h1>
+      <h1 onClick={handleClick}>{name}</h1>
       <HamburgerMenu />
     </header>
   );
---
+++ src/components/TriforceSVG.js
@@ -0,0 +1,20 @@
+// src/components/TriforceSVG.js
+import React from 'react';
+import '../assets/css/TriforceSVG.css';
+
+const TriforceSVG = () => (
+  <svg
+    className="triforce-svg"
+    width="100"
+    height="100"
+    viewBox="0 0 100 100"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    {/* Adjusted points for all triangles to ensure they fit within the viewBox */}
+    <polygon className="triforce-triangle" points="50,20 30,60 70,60" /> {/* Top Triangle */}
+    <polygon className="triforce-triangle" points="30,60 5,110 55,110" /> {/* Bottom Left Triangle */}
+    <polygon className="triforce-triangle" points="70,60 45,110 95,110" /> {/* Bottom Right Triangle */}
+  </svg>
+);
+
+export default TriforceSVG;
