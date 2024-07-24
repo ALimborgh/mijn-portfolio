21a5271ca6473eba1d17dc82bc0fe82f3fe11ae9
Adriaen Limborgh <adriaen95@hotmail.com>
2024-07-24 12:31:24 +0200

    feat: Add CLI Project to projects data
    This commit adds the CLI Project to the projects data in the ProjectsData.js file. The CLI Project is a command-line interface (CLI) for a supermarket management system called 'superpy'. It allows users to perform various subcommands such as buying and selling products, listing products, moving forward or backward in time, calculating profit and revenue, and resetting the date. The project uses Python, CLI, argparse, rich, datetime, CSV, OOP, unittest, Pytest, timedelta, time, os, rich.console, rich.table, and rich.progress. The CLI Project is applicable for inventory management, profit and revenue calculation, expiration date management, date management, automated reporting and analysis, and training environments. The commit also includes the necessary image and a link to the project repository.

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -6,6 +6,7 @@ import advancedReactProject2 from '../assets/images/Advanced React Project 2.png
 import advancedReactProject3 from '../assets/images/Advanced React Project 3.png';
 import advancedReactProject4 from '../assets/images/Advanced React Project 4.png';
 import advancedReactProject5 from '../assets/images/Advanced React Project 5.png';
+import cliProject from '../assets/images/CLI Project.png';
 
 const projectsData = [
     {
@@ -35,7 +36,15 @@ const projectsData = [
     ],
       link: 'https://github.com/ALimborgh/React-Advanced-Project',
     },
-    // Add more projects as needed
+    {
+      id: 3,
+      title: 'CLI Project (Schoolopdracht)',
+      description: "Deze code vormt een command-line interface (CLI) voor een supermarktbeheersysteem genaamd 'superpy'. Het gebruikt argparse om verschillende subcommando's te parseren en verwerken zoals buy, sell, list, forward, backward, profit, revenue en reset_date. Gebruikers kunnen producten kopen of verkopen, een lijst van producten bekijken, de tijd vooruit of achteruit reizen om bijvoorbeeld verlopen producten te verwijderen, en de winst of omzet op een bepaalde datum berekenen. De TimeMachine-klasse beheert de datum en tijd, terwijl functies zoals buy_product en sell_product producten toevoegen aan of verwijderen uit CSV-bestanden. Visuele feedback wordt gegeven met behulp van de rich bibliotheek.",
+      Toepassingen: ['Voorraadbeheer', 'Winst- en omzetberekening', 'Vervaldatumanagement', 'Datumbeheer', 'Geautomatiseerde rapportage en analyses', 'Gebruik in trainingsomgevingen'],
+      technologies: ['Python', 'CLI', 'argparse', 'rich', 'datetime', 'CSV', 'OOP', 'unittest', 'Pytest', 'timedelta', 'time', 'os', 'rich.console', 'rich.table', 'rich.progress'],
+      imagePaths: [cliProject],
+      link: 'https://github.com/ALimborgh/SuperPy',
+    },
   ];
   
   export default projectsData;
